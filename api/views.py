ecd0d171f7893292fc9647777f4e7d6211a4d981
Casey Thomas <cpthomas@gmail.com>
2012-07-01 17:02:51 -0400

    Second iteration of issue d3 chart

--- api/views.py
+++ api/views.py
@@ -7,29 +7,39 @@ def index(request, pk):
     issue = get_object_or_404(Issue, pk=pk)
 
     response = {
-        #'name': issue.description,
-        'nodes': [
-            { 
-                'id': 0,
-                'type': 'issue',
-                'name': issue.description, 
-            },
-        ],
+        'nodes': [],
         'links': []
     }
 
     for i, x in enumerate(issue.exp_direct_comm_set.all()): 
-        response['nodes'].append({
-                'type': 'principal',
-                'name': x.filing.principal.name,
-                'position': x.position,
-                'id': x.filing.principal.pk,
-                'group': x.filing.principal.pk
-            })
+        
+        p = {
+            'type': 'principal',
+            'name': x.filing.principal.name,
+            'position': x.position,
+            'size': 10
+        }
 
-        response['links'].append({
-                'source': i + 1,
-                'target': 0,
+        response['nodes'].append(p)
+
+        links = []
+
+        for c, o in enumerate(x.officials.all()):
+            item = {
+                'type': 'official',
+                'last_name': o.last_name,
+                'size': 5
+            }
+
+            if item not in response['nodes']:
+                response['nodes'].append(item)
+
+            links.append(response['nodes'].index(item))
+
+        for link in links: 
+            response['links'].append({
+                'source': response['nodes'].index(p),
+                'target': link,
                 'value': 10
             })
 
