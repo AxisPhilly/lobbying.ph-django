1459bb86fad89c8113b8763055f3847b93773e01
Casey Thomas <cpthomas@gmail.com>
2012-07-04 15:55:50 -0400

    Chaning lobbyist.principal to manytomany since some lobbyists work for multiple principals.

--- lobbyingph/models.py
+++ lobbyingph/models.py
@@ -34,7 +34,7 @@ class Lobbyist(models.Model):
     email = models.CharField(max_length=75)
 
     firm = models.ForeignKey('Firm', null=True, blank=True)
-    principal = models.ForeignKey('Principal', null=True, blank=True)
+    principal = models.ManyToManyField('Principal', null=True, blank=True)
 
     class Meta:
         ordering = ['name']
