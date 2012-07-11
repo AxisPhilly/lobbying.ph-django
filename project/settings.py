83de3701b04b6a2811c1e1cb5ff56390079fd908
Casey Thomas <cpthomas@gmail.com>
2012-07-11 15:13:50 -0400

    Anther settings tweak

--- project/settings.py
+++ project/settings.py
@@ -36,8 +36,8 @@ DATABASES = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': os.environ['BONSAI_INDEX_URL'],
-        'INDEX_NAME': '',
+        'URL': os.environ['BONSAI_URL'],
+        'INDEX_NAME': os.environ['BONSAI_INDEX'],
         'EXCLUDED_INDEXES': [
             'lobbyingph.search_indexes.PrincipalIndex',
         ]
