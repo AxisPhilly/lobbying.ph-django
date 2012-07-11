b5c2a12a4753d25b773e405dfa79f473d390db9b
Casey Thomas <cpthomas@gmail.com>
2012-07-11 12:40:14 -0400

    Reverting to prevous version of search_indexes

--- search/search_indexes.py
+++ search/search_indexes.py
@@ -24,3 +24,13 @@ class FirmIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
 
+class LobbyistIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Lobbyist
+
+    def index_queryset(self):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
