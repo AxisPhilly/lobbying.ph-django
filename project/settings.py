df0007abfd9393b1f2ed99279ce680be2bf846b9
Casey Thomas <cpthomas@gmail.com>
2012-07-15 22:26:15 -0400

    Turning off debug

--- project/settings.py
+++ project/settings.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = dirname(dirname(abspath(__file__)))
 PROJECT_URL = '/'
 SETTINGS_DIR = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 INTERNAL_IPS = (
     '127.0.0.1'
@@ -89,7 +89,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_ROOT + 'project/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
