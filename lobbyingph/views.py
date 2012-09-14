a442ef2986d6445b7084470adb021d3face4dbf5
Casey Thomas <cpthomas@gmail.com>
2012-09-14 17:09:26 -0400

    pylinted

--- lobbyingph/views.py
+++ lobbyingph/views.py
@@ -1,22 +1,20 @@
+"""
+View for Lobbyist, Firm, Principal and site index page
+"""
 from __future__ import division
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from lobbyingph.models import Lobbyist, Firm, Principal, Issue, Filing, Bill
-from django.db.models import Sum
-from django.shortcuts import get_object_or_404, render, render_to_response
-from decimal import *
-from django.db import connection
+from lobbyingph.models import Lobbyist, Firm, Principal, Issue, Filing
+from django.shortcuts import render
 import simplejson as json
 
+
 def index(request):
-    lobbyists = Lobbyist.objects.all()
+    """
+    Site index view
+    """
     firms = Firm.objects.all()
     principals = Principal.objects.all()
-
-    # Count 'em up
-    lobbyist_count = lobbyists.count()
-    firm_count = firms.count()
-    principal_count = principals.count()
 
     # Count up all the money
     filings = Filing.objects.all()
@@ -25,41 +23,47 @@ def index(request):
         total_spending += filing.get_total_exp()
 
     # Sort the base lists
-    # http://stackoverflow.com/questions/930865/how-to-sort-by-a-computed-value-in-django
-    firms_sorted_clients = sorted(firms, key = lambda f: -f.get_client_count())
-    p_sorted_spending = sorted(principals, key = lambda p: -p.get_total_exp())
-    p_sorted_issue_bill = sorted(principals, key = lambda p: -p.get_issue_bill_count())
+    # stackoverflow.com/questions/930865/
+    firms_sorted_clients = sorted(firms,
+        key=lambda f: -f.get_client_count())
+    p_sorted_spending = sorted(principals,
+        key=lambda p: -p.get_total_exp())
+    p_sorted_issue_bill = sorted(principals,
+        key=lambda p: -p.get_issue_bill_count())
 
     # Then generate top lists
-    # http://stackoverflow.com/questions/5306756/how-to-show-percentage-in-python
+    # http://stackoverflow.com/questions/5306756/
     top_firms = []
-    for f in firms_sorted_clients[:5]:
+    for firm in firms_sorted_clients[:5]:
         top_firms.append({
-            'object': f,
-            'count': f.get_client_count(),
-            'percent': "{:.0%}".format(f.get_client_count()/firms_sorted_clients[0].get_client_count())
+            'object': firm,
+            'count': firm.get_client_count(),
+            'percent': "{:.0%}".format(firm.get_client_count() /
+                firms_sorted_clients[0].get_client_count())
         })
 
     top_p_spending = []
-    for p in p_sorted_spending[:5]:
+    for principal in p_sorted_spending[:5]:
         top_p_spending.append({
-            'object': p,
-            'count': p.get_total_exp(),
-            'percent': "{:.0%}".format(p.get_total_exp()/p_sorted_spending[0].get_total_exp())
+            'object': principal,
+            'count': principal.get_total_exp(),
+            'percent': "{:.0%}".format(principal.get_total_exp() /
+                p_sorted_spending[0].get_total_exp())
         })
 
     top_p_issue_bill = []
-    for p in p_sorted_issue_bill[:5]:
+    for principal in p_sorted_issue_bill[:5]:
         top_p_issue_bill.append({
-            'object': p,
-            'count': p.get_issue_bill_count(),
-            'percent': "{:.0%}".format(p.get_issue_bill_count()/p_sorted_issue_bill[0].get_issue_bill_count())
+            'object': principal,
+            'count': principal.get_issue_bill_count(),
+            'percent': "{:.0%}".format(principal.get_issue_bill_count() /
+                p_sorted_issue_bill[0].get_issue_bill_count())
         })
 
     context = {
-        'lobbyist_count': lobbyist_count,
-        'firm_count': firm_count,
-        'principal_count': principal_count,
+        'lobbyist_count': Lobbyist.objects.count(),
+        'firm_count': firms.count(),
+        'principal_count': principals.count(),
         'total_spending': total_spending,
         'top_firms': top_firms,
         'top_principals_by_spending': top_p_spending,
@@ -68,31 +72,59 @@ def index(request):
 
     return render(request, 'index.html', context)
 
+
 class IssueDetail(DetailView):
+    """
+    Issue/Bill detail view
+    """
     model = Issue
     template_name = 'issue_detail.html'
 
+
 class LobbyistList(ListView):
+    """
+    Table/list view of Lobbyists
+    """
     model = Lobbyist
     template_name = 'lobbyist_list.html'
 
+
 class LobbyistDetail(DetailView):
+    """
+    Detail view of Lobbyist
+    """
     model = Lobbyist
     template_name = 'lobbyist_detail.html'
 
+
 class FirmList(ListView):
+    """
+    Table/List view of Firm
+    """
     model = Firm
     template_name = 'firm_list.html'
 
+
 class FirmDetail(DetailView):
+    """
+    Detail view of Firm
+    """
     model = Firm
     template_name = 'firm_detail.html'
 
+
 class PrincipalList(ListView):
+    """
+    Table/List view of Principal
+    """
     model = Principal
     template_name = 'principal_list.html'
 
+
 class PrincipalDetail(DetailView):
+    """
+    Detail view of Principal
+    """
     model = Principal
     template_name = 'principal_detail.html'
 
@@ -106,29 +138,30 @@ class PrincipalDetail(DetailView):
         exp_data = {}
         exp_data['total'] = []
         percents = self.object.get_exp_percents()
-        for k,v in self.object.get_exp_totals().items():
+        for exp_type, dollars in self.object.get_exp_totals().items():
             exp_data['total'].append(
             {
-                'class': k,
-                'dollars': v, 
-                'percent': percents[k]
-            });
+                'class': exp_type,
+                'dollars': dollars,
+                'percent': percents[exp_type]
+            })
 
         # Then add the expenditures for each quarter
         q_percents = self.object.get_exp_percents_by_quarter()
-        for k,v in self.object.get_exp_totals_by_quarter().items():
-            exp_data[k] = []
-            for qk, qv in v.items():
-                exp_data[k].append({
-                    'class': qk,
-                    'dollars': qv, 
-                    'percent': q_percents[k][qk]
-                });
+        for quarter, totals in self.object.get_exp_totals_by_quarter().items():
+            exp_data[quarter] = []
+            for exp_type, dollars in totals.items():
+                exp_data[quarter].append({
+                    'class': exp_type,
+                    'dollars': dollars,
+                    'percent': q_percents[quarter][exp_type]
+                })
 
         context['d3_data'] = json.dumps(exp_data)
 
         context['exp_totals'] = self.object.get_exp_totals()
         context['exp_percents'] = self.object.get_exp_percents()
-        context['quarters'] = self.object.filing_set.distinct('quarter','year').values('quarter','year')
-        
+        context['quarters'] = self.object.filing_set.distinct(
+            'quarter', 'year').values('quarter', 'year')
+
         return context
